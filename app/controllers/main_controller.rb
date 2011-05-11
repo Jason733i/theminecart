33a66094c52d94d769c5e3fc20568f7cb18ccf8e
Jason Berry <uniqueberry@gmail.com>
2011-05-10 21:54:52 -0400

    Testing commit

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
   def index
     @SHIT = Array.new
-    (1..45).each do |i|
+    (1..30).each do |i|
       @SHIT << "I took #{i} shits!"
     end 
   end
