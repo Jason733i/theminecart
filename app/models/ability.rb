5f44547d42f10b38809d73fe6bf863d29e2be30a
Jason Berry <uniqueberry@gmail.com>
2011-05-12 23:22:51 -0400

    First attempt at specifying a user's abilities.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
+    if user.superadmin?
+      can :manage, :all
+    else if user.admin?
+      can [:create, :update, :read], :all
+    else if user.moderator?
+      can [:update, :read], :all
+    else if user.member?
+      can :read, :all
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
